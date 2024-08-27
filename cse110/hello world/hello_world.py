cef168ae89732f15600027f821446f47af46797c
EskolaHJ <esk23003@byui.edu>
2024-08-27 14:56:28 -0600

    Relearning some python

---
+++ cse110/hello world/hello_world.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+print('Hello')
+print(name)
+
+print('Hello world!')
+
+print('Hello \nWorld tomorrow')
---
+++ cse110/week01/main.py
@@ -0,0 +1,7 @@
+color = input('What is your favorite snack?')
+print(color)
+print('No way that is one of my favorites too!!')
+
+name = input('What is you name?')
+print(name)
+print('\nMy name is Jacob it is a pleasure to meet you')
