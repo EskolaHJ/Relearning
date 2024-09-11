be7194b5a052b588ca51160adef02297a0496f81
EskolaHJ <esk23003@byui.edu>
2024-09-10 22:45:00 -0600

    fixed line 21 was missing the images/ in the src function. along with lines 30 and 31 put the ( ) onto the setAttribute

--- cse121b/w02-task/scripts/w02-task.js
+++ cse121b/w02-task/scripts/w02-task.js
@@ -6,22 +6,29 @@
 // setting the variable full name to the value of my name
 let fullName = 'Jacob Eskola';
 // setting currentYear variable to the value of Date
-let currentYear = Date();
+let currentYear = new Date();
 // setting profilePicture variable to the value of images/ProfilePicture.png
 let profilePicture = 'images/ProfilePicture.png';
 
 /* Step 3 - Element Variables */
+// Get the HTML element with the ID 'name' and assigning it to the variable 'nameElement'
 const nameElement = document.getElementById('name');
+// Get  the first HTML element that matches the CSS selector '#year'
+// and assign it to the variable 'yearElement'
 const yearElement = document.querySelector('#year');
-let imageElement = document.querySelector('img');
+// Get the first <img> element that has a 'src' attribute with the value of 'placeholder.png'
+// and assign it to the variable imageElement
+let imageElement = document.querySelector("img[src='images/placeholder.png']");
 
 
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong> ${fullName}</strong>`;
-yearElement.textContent = `${currentYear}`;
-imageElement.setAttribute = ('src', profilePicture);
-imageElement.setAttribute = ('alt', 'Picture of Jacob Eskola')
+// Set the inner HTML of 'nameElement' to display the fullName variable with a bold style
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+// Set the text content of 'yearElement' to the current year (note: you may want just the year part, e.g., currentYear.getFullYear())
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Picture of ${fullName}`);
 
 
 
